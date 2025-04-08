1d8b132b9613ed402da8f336fbca7dc30db7dde8
PedroCastro729 <pedromigulopolis@gmail.com>
2025-04-08 20:09:42 -0300

    Add files via upload

---
+++ ex8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int testa_par(int);
+
+int main (void) {
+    int valor;
+    printf("Digite um valor: ");
+    scanf("%d", &valor);
+    
+    printf("%d par? %d", valor, testa_par(valor));
+    
+    return 0;
+}
+
+int testa_par(int valor) {
+    if(valor % 2 == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
